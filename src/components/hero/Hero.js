850857464304ff9a894bcf96e8707d5a9ff6a0c6
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2022-08-31 23:35:07 +0530

    some code correction
    Signed-off-by: Utkarsh Kumar Sharma <utkarshkviim@gmail.com>

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <div className='hero'>
             <div className='content'>
-                <h1>Find the perfect place</h1>
+                <h1>Find the dream place</h1>
                 <p className='search-text'> Search the largest selection of luxury high-rise apartments, multi-family homes, and luxury homes. </p>
                 <form className='search'>
 
